c03d9e42f8f572f43f045e6903f9ee5c76146dba
Jecy2305 <114971923+Jecy2305@users.noreply.github.com>
2023-05-04 20:10:14 -0500

    Update Pomodoro

--- Program.cs
+++ Program.cs
@@ -16,7 +16,9 @@ namespace Version2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PomodoroContador());
             Application.Run(new Form1());
+            
         }
     }
 }
